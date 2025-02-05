c142191ef764121a1c4cc7a5aae607b5475e1fbc
Udhaykrishn <krishnauday320@gmail.com>
2025-02-05 19:57:46 +0530

    chore: delete products router and products home page files

--- src/routes/vendor/product/index.lazy.tsx
+++
@@ -1,9 +0,0 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
-
-export const Route = createLazyFileRoute('/vendor/product/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/vendor/product/"!</div>
-}
