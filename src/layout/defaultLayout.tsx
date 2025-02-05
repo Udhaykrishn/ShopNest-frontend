578f5430c4c40be33f9646e6fa3db0478a9e3621
Udhaykrishn <krishnauday320@gmail.com>
2025-02-05 19:54:34 +0530

    feat: update the defaultLayout file

--- src/layout/defaultLayout.tsx
+++ src/layout/defaultLayout.tsx
@@ -1,17 +1,27 @@
 import { Footer } from "@/components/Footer"
 import { Navbar } from "@/components/Navbar"
 import { ScrollToTop } from "@/components/ScrollToTop"
-import { ReactNode } from "react"
-import { Outlet } from "@tanstack/react-router"
+import { ReactElement } from "react"
+import { Outlet, useRouter } from "@tanstack/react-router"
 import { FAQ } from "@/components/FAQ"
 
-const DefaultLayout = ({ children }: { children: ReactNode }) => {
+
+
+
+const DefaultLayout = ({ children }: { children: ReactElement }) => {
+    const router = useRouter();
+    const pathname = router.state.location.pathname;
+    const isVendorOrAdmin = pathname.startsWith("/vendor") || pathname.startsWith("/admin")
+
+    if (isVendorOrAdmin) {
+        return <Outlet />
+    }
     return (
+
         <div className="relative flex flex-col h-screen">
             <Navbar />
             <main className="container max-w-7xl px-6 flex-grow pt-0">
-                {children}
-                <Outlet />
+                {children || <Outlet />}
             </main>
             <footer className="w-full pl-3
             ">
@@ -20,6 +30,7 @@ const DefaultLayout = ({ children }: { children: ReactNode }) => {
                 <ScrollToTop />
             </footer>
         </div>
+
     )
 }
 
