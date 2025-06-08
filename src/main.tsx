bb28982980171b6f4c7d83f6703fe45bb6457bd6
UdhayKrishn <krishnauday320@gmail.com>
2025-06-08 13:14:57 +0530

    feat: tanstack router and provided updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,31 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
 import "./index.css"
 import "./App.css"
-// Import the generated route tree
-import { routeTree } from './routeTree.gen'
 import { ThemeProvider } from './components/theme-provider';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { Toaster } from '@/components/ui/toaster'
+import { App } from "@/App"
 
-// Create a new router instance
-const router = createRouter({ routeTree })
-
-// Register the router instance for type safety
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
+export const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    }
   }
-}
+});
 
-// Render the app
 const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <ThemeProvider>
-        <RouterProvider router={router} />
-      </ThemeProvider>
+      <QueryClientProvider client={client}>
+        <ThemeProvider>
+          <Toaster />
+          <App  />
+        </ThemeProvider>
+      </QueryClientProvider>
     </StrictMode>,
   )
 }
