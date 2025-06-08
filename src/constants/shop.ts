a39d3e00c4a148fa4c0220d8951266bbca3c9bb4
UdhayKrishn <krishnauday320@gmail.com>
2025-06-08 13:22:22 +0530

    feat: added constant file

---
+++ src/constants/shop.ts
@@ -0,0 +1,26 @@
+export const CATEGORIES = [
+    "All",
+    "Electronics",
+    "Clothing",
+    "Books",
+    "Home & Kitchen",
+    "Toys",
+] as const;
+
+export const CAROUSEL_IMAGES = [
+    {
+        url: "https://plus.unsplash.com/premium_photo-1661606444346-2c61e8b3bd7f?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Summer Collection",
+        subtitle: "Explore our latest arrivals"
+    },
+    {
+        url: "https://images.unsplash.com/photo-1519389950473-47ba0277781c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Tech Deals",
+        subtitle: "Up to 40% off on electronics"
+    },
+    {
+        url: "https://images.unsplash.com/photo-1576503276229-998652dc7e56?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Home Essentials",
+        subtitle: "Transform your living space"
+    }
+] as const;
