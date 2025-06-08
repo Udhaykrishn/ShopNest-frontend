dc7cdbddf7864bdac364b235a13338c651039cc5
UdhayKrishn <krishnauday320@gmail.com>
2025-06-08 13:12:21 +0530

    feat: modified tailwind and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import path from "path";
-import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
+import react from "@vitejs/plugin-react-swc";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), , react()],
+  
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    allowedHosts: true,
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
   },
 });
