a0876e457c8634de56bf592620b88745a0435ac2
UdhayKrishn <krishnauday320@gmail.com>
2025-06-08 13:21:24 +0530

    feat: layout structure

--- src/layout/defaultLayout.tsx
+++ src/layout/defaultLayout.tsx
@@ -1,37 +1,29 @@
-import { Footer } from "@/components/Footer"
 import { Navbar } from "@/components/Navbar"
 import { ScrollToTop } from "@/components/ScrollToTop"
-import { ReactElement } from "react"
 import { Outlet, useRouter } from "@tanstack/react-router"
-import { FAQ } from "@/components/FAQ"
 
+const DefaultLayout = () => {
+  const {
+    state: {
+      location: { pathname },
+    },
+  } = useRouter()
 
+  const isVendorOrAdmin = /^\/(vendor|admin|login|signup|forgot(-password-otp)?|password)/.test(pathname)
 
-
-const DefaultLayout = ({ children }: { children: ReactElement }) => {
-    const router = useRouter();
-    const pathname = router.state.location.pathname;
-    const isVendorOrAdmin = pathname.startsWith("/vendor") || pathname.startsWith("/admin")
-
-    if (isVendorOrAdmin) {
-        return <Outlet />
-    }
-    return (
-
-        <div className="relative flex flex-col h-screen">
-            <Navbar />
-            <main className="container max-w-7xl px-6 flex-grow pt-0">
-                {children || <Outlet />}
-            </main>
-            <footer className="w-full pl-3
-            ">
-                <FAQ />
-                <Footer />
-                <ScrollToTop />
-            </footer>
-        </div>
-
-    )
+  return isVendorOrAdmin ? (
+    <Outlet />
+  ) : (
+    <div className="relative flex flex-col h-screen">
+      <Navbar />
+      <main className="container max-w-7xl px-6 flex-grow pt-0">
+        <Outlet />
+      </main>
+      <footer className="w-full pl-3">
+        <ScrollToTop />
+      </footer>
+    </div>
+  )
 }
 
 export default DefaultLayout
